8c43b1efb44025f7f83a19a4a31052217e4394d5
Julian Kleine <info@lxdriver.de>
2014-01-09 16:24:54 +0100

    Reimported

---
+++ Assets/DroneController.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DroneController : MonoBehaviour {
+	
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	int speed = 5;
+
+	void Update () {
+		if(Input.GetKey("w")) {
+			transform.Translate(Vector3.forward * Time.deltaTime * speed);
+		}
+		if(Input.GetKey("s")) {
+			transform.Translate(Vector3.back * Time.deltaTime * speed);
+		}
+		if(Input.GetKey("a")) {
+			transform.Translate(Vector3.left * Time.deltaTime * speed);
+		}
+		if(Input.GetKey("d")) {
+			transform.Translate(Vector3.right * Time.deltaTime * speed);
+		}
+	}
+}
