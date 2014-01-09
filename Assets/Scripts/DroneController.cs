2c1c865b0cde1eecec2e7ebd885ceb8cd729911e
Julian Kleine <info@lxdriver.de>
2014-01-09 17:49:57 +0100

    InputController

--- Assets/Scripts/DroneController.cs
+++ Assets/Scripts/DroneController.cs
@@ -16,48 +16,6 @@ public class DroneController : MonoBehaviour {
 
 
 	void Update () {
-      /*  if (Input.GetKey("w") && speedfwd <= 5)
-        {
-            speedfwd++;
-        }
-        else if (speedfwd > 0)
-        { speedfwd=speedfwd - 0.1f; }
-        //else
-        //{ speedfwd = 0; }
-
-        if (Input.GetKey("s") && speedbwd <= 5)
-        {
-            speedbwd++;
-        }
-        else if (speedbwd > 0)
-        { speedbwd=speedbwd -0.1f; }
-        //else
-        //{ speedbwd = 0; }
-
-        if (Input.GetKey("a") && speedleft <= 5)
-        {
-            speedleft++;
-        }
-        else if (speedleft > 0)
-        { speedleft=speedleft - 0.1f; }
-        //else
-        //{ speedleft = 0; }
-
-        if (Input.GetKey("d") && speedright <= 5)
-        {
-            speedright++;
-        }
-        else if (speedright > 0)
-        { speedright= speedright - 0.1f; }
-        //else
-        //{ speedright = 0; }
-		
-
-        transform.Translate(Vector3.forward * Time.deltaTime * speedfwd);
-        transform.Translate(Vector3.back * Time.deltaTime * speedbwd);
-        transform.Translate(Vector3.left * Time.deltaTime * speedleft);
-        transform.Translate(Vector3.right * Time.deltaTime * speedright);*/
-
         if (Input.GetKey("w"))
         { this.rigidbody.AddForce(Vector3.forward * 10); }
         if (Input.GetKey("s"))
---
+++ Assets/Scripts/InputController.cs
@@ -0,0 +1,65 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.IO.Ports;
+using System;
+
+public class InputController : MonoBehaviour {
+	SerialPort stream = new SerialPort("COM3", 115200);
+	string receivedData = "";
+	
+	public GameObject player1;
+	public GameObject player2;
+	
+	
+	float prevNormRV3;
+	float prevNormRV4;
+	
+	float normRV3;
+	float normRV4;
+	
+	int bla ;
+	// Use this for initialization
+	public void Start () {
+		stream.Open(); // Open the serial stream
+		Debug.Log("Serial Port opened.");
+	}
+	
+	// Update is called once per frame
+	public void Update () {
+		stream.Write ("4");
+		receivedData = stream.ReadLine();
+		
+		string[] dataArray = receivedData.Split(' ');
+		
+		int RV1 = System.Convert.ToInt32(dataArray[1],16);
+		int RV2 = System.Convert.ToInt32(dataArray[2],16);
+		int RV3 = System.Convert.ToInt32(dataArray[3],16);
+		int RV4 = System.Convert.ToInt32(dataArray[4],16);
+		
+		float normRV1 = ((float)(RV1) / 4095.0f);
+		float normRV2 = ((float)(RV2) / 4095.0f);
+		
+		prevNormRV3 = normRV3;
+		prevNormRV4 = normRV4;
+		
+		normRV3 = ((float)(RV3) / 2047.0f) - 1.0f;
+		normRV4 = ((float)(RV4) / 2047.0f) - 1.0f;
+		
+		float newPosYRV3 = normRV3 * 100; 
+		float newPosYRV4 = normRV4 * 100;
+		
+		Debug.Log("RV 3: " + newPosYRV3 + " RV4: " + newPosYRV4);
+		
+		player1.SendMessage("setPosY",newPosYRV4);
+		player2.SendMessage("setPosY",newPosYRV3);
+	}
+	
+	void OnGUI () {
+		//GUI.Box(new Rect(300,10,300,90), receivedData);
+	}
+	
+	public void GetSliderState() {
+		Debug.Log(receivedData);
+	}
+	
+}
