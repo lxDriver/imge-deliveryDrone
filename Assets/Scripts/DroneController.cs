ec50d10677fe4927ab983605ddd12e315535d6b1
Kleine Julian <julian.kleine@tum.de>
2014-01-10 13:44:19 +0100

    Animated Drone & worked on Drone Controller

--- Assets/Scripts/DroneController.cs
+++ Assets/Scripts/DroneController.cs
@@ -9,10 +9,12 @@ public class DroneController : MonoBehaviour {
 
 	void Update () {
 		//	@TODO: Add rotation to drone, depending on direction  
+		Vector3 direction = this.rigidbody.transform.position - transform.position;
         if (Input.GetKey("w"))
         { 
 			// move forward
 			this.rigidbody.AddForce(Vector3.forward * 10); 
+			//this.transform.RotateAround(this.transform.position, Vector3.up, 1);
 		}
         if (Input.GetKey("s"))
         { 	
